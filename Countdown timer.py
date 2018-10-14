4a9b937733c2ce9d182bd135e410b2075280d0b4
Parth Kandpal <42544546+parthkandpal@users.noreply.github.com>
2018-10-14 18:47:42 +0530

    Add files via upload

---
+++ Countdown timer.py
@@ -0,0 +1,26 @@
+import subprocess, time
+#subprocess.Popen('C:\\Windows\\System32\\calc.exe')
+
+print("For how much seconds you want to set countdown?")
+countdown=int(input())
+
+print("Staring Countdown.........")
+time.sleep(1)
+print("You ready...")
+time.sleep(1)
+print("1")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("3")
+time.sleep(2)
+print("Let's Go")
+
+for i in range(1,countdown+1):
+
+    time.sleep(1)
+    print("Countdown...here {}".format(i))
+
+print("Countdown Finished!")
+
+subprocess.Popen(['start', 'alarm.wav'], shell=True)
